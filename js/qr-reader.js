edc81ba04791299a7a79f0efcb09674175d6e16b
JinminZZang <yangjinmin95@gmail.com>
2022-07-14 17:04:51 +0900

    asyaaf

--- js/qr-reader.js
+++ js/qr-reader.js
@@ -6,8 +6,8 @@ scanActiveBtn.addEventListener("click", function () {
     var canvas = canvasElement.getContext("2d");
     var loadingMessage = document.getElementById("loadingMessage");
     // var outputContainer = document.getElementById("output");
-    var outputMessage = document.getElementById("outputMessage");
-    var outputData = document.getElementById("outputData");
+    // var outputMessage = document.getElementById("outputMessage");
+    // var outputData = document.getElementById("outputData");
 
     function drawLine(begin, end, color) {
         canvas.beginPath();
@@ -53,15 +53,15 @@ scanActiveBtn.addEventListener("click", function () {
                 drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF0000");
                 drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF0000");
 
-                outputMessage.hidden = true;
-                outputData.parentElement.hidden = false;
+                // outputMessage.hidden = true;
+                // outputData.parentElement.hidden = false;
 
                 // QR코드 메시지 출력
-                const walletAddress = code.data.replace('ethereum:','');    // code text를 형식에 맞게 치환
-                outputData.innerText = walletAddress;
-
+                const walletAddressCode = code.data.replace('ethereum:','');    // code text를 형식에 맞게 치환
+                // outputData.innerText = walletAddress;
+                walletAddressCode
                 const walletAddressInput = document.querySelector('#walletAddress');
-                walletAddressInput.value = walletAddress;
+                walletAddressInput.value = walletAddressCode;
                 // outputDataLength.innerText = walletAddress.length;
 
                 // 스캔 기능 비활성화
@@ -70,13 +70,16 @@ scanActiveBtn.addEventListener("click", function () {
                 // outputContainer.hidden = false;
 
                 // return을 써서 함수를 빠져나가면 QR코드 프로그램이 종료된다.
+                var myModalEl = document.getElementById('exampleModal');
+                var modal = bootstrap.Modal.getInstance(myModalEl);
+                modal.hide();
                 return;
             }
 
             // QR코드 인식에 실패한 경우
             else {
                 // outputMessage.hidden = false;
-                // outputData.parentElement.hidden = true; 
+                // outputData.parentElement.hidden = true;
             }
         }
         requestAnimationFrame(tick);
